869e7dc15420238ee67ed1b53aca604b0515c433
Bruno Bulacia <beueleceia@gmail.com>
2025-09-14 14:05:17 -0400

    INSCRIBIR A ESTUDIANTE YA HECHO SINCRONICAMENTE

--- src/transactions/inscripcion/inscripcion.service.ts
+++ src/transactions/inscripcion/inscripcion.service.ts
@@ -10,7 +10,7 @@ import { GrupoMateria, Prisma } from '@prisma/client';
 export class InscripcionService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  /* async inscripcion(createInscripcionDto: CreateInscripcionDto) {
+  async inscripcion(createInscripcionDto: CreateInscripcionDto) {
     return await this.prismaService.$transaction(async (prisma) => {
       //VALIDANDO SI EL REGISTRO LE PERTENECE A ALGUN ESTUDIANTE
       const foundStudent = await prisma.estudiante.findUnique({
@@ -20,34 +20,16 @@ export class InscripcionService {
         throw new NotFoundException('Estudiante no encontrado');
       }
 
-      //VIENDO SI EL ESTUDIANTE TIENE ALGUNA FICHA YA CREADA
-      const foundInscripcionId = await prisma.fichaInscripcion.findFirst({
-        where: { estudianteId: foundStudent.id },
+      const createFichaInscripcion = await prisma.fichaInscripcion.create({
+        data: {
+          estudianteId: foundStudent.id,
+        },
       });
-
-      let fichaInscripcionId: string;
-
-      //SI EL ESTUDIANTE NO TIENE FICHA DE INSCRIPCION, LE CREAMOS UNA
-      if (!foundInscripcionId) {
-        const createFichaInscripcion = await prisma.fichaInscripcion.create({
-          data: {
-            estudianteId: foundStudent.id,
-          },
-        });
-
-        if (!createFichaInscripcion) {
-          throw new NotAcceptableException('Error al crear la ficha');
-        }
-
-        fichaInscripcionId = createFichaInscripcion.id;
-      } else {
-        fichaInscripcionId = foundInscripcionId.id;
-      }
 
       //DELEGAMOS A UNA FUNCION PARA QUE INSCRIBA LAS MATERIAS
       return await this.crearDetallesDeInscripcionEnTransaccion(
         createInscripcionDto,
-        fichaInscripcionId,
+        createFichaInscripcion.id,
         foundStudent.id,
         prisma,
       );
@@ -75,19 +57,23 @@ export class InscripcionService {
     }
 
     // BUSCAMOS LAS MATERIAS QUE QUEREMOS INSCRIBIR
-    const materias = await prisma.grupoMateria.findMany({
+    const materias = await prisma.ofertaGrupoMateria.findMany({
       where: {
-        id: { in: createInscripcionDto.materiasId },
+        grupoMateriaId: { in: createInscripcionDto.materiasId },
       },
+      include: { GrupoMateria: true },
     });
 
     //VALIDAR SI EL ESTUDIANTE QUIERE INSCRIBIR MAS DE 7 MATERIAS
-    if (
-      materias.length !== createInscripcionDto.materiasId.length ||
-      materias.length > 7
-    ) {
+    if (materias.length !== createInscripcionDto.materiasId.length) {
       throw new NotFoundException(
         'Alguna de las materias seleccionadas no existe',
+      );
+    }
+
+    if (materias.length > 7) {
+      throw new NotAcceptableException(
+        'No se pueden inscribir más de 7 materias en una sola ficha de inscripción',
       );
     }
 
@@ -108,15 +94,21 @@ export class InscripcionService {
         (bgm) => bgm.grupoMateriaId,
       );
 
+      console.log('Materias ya inscritas del estudiante:');
+      console.log(materiasYaInscritas);
+
       const materiasConflicto = materias.filter((materia) =>
-        materiasYaInscritas.includes(materia.id),
+        materiasYaInscritas.includes(materia.grupoMateriaId),
       );
+
+      console.log('Materias en conflicto:');
+      console.log(materiasConflicto);
 
       if (materiasConflicto.length > 0) {
         const nombresMateriasConflicto = await prisma.materia.findMany({
           where: {
             id: {
-              in: materiasConflicto.map((mc) => mc.materiaId),
+              in: materiasConflicto.map((mc) => mc.grupoMateriaId),
             },
           },
           select: {
@@ -135,10 +127,10 @@ export class InscripcionService {
 
     // FILTRAMOS LAS MATERIAS CON CUPO DISPONIBLE Y DE UNA CONSTRUIMOS EL PAYLOAD
     const payload = materias
-      .filter((materia) => materia.cupos > 0)
+      .filter((materia) => materia.GrupoMateria.cupos > 0)
       .map((materia) => ({
         detalleInscripcionId: createDetalleInscripcion.id,
-        grupoMateriaId: materia.id,
+        ofertaGrupoMateriaId: materia.id,
         estado: 'INSCRITA',
       }));
 
@@ -148,12 +140,10 @@ export class InscripcionService {
 
     console.log(payload);
 
-    //INSCRIBIMOS TODAS LAS MATERIAS DE UN SAQUE
-    const createDetalleInsGrupoMat = await prisma.detalleInsGrupoMat.createMany(
-      {
-        data: payload,
-      },
-    );
+    //INSCRIBIMOS TODAS LAS MATERIAS DE UN SAQUE PERRITO
+    const createDetalleInsGrupoMat = await prisma.detalleInsOferta.createMany({
+      data: payload,
+    });
 
     if (createDetalleInsGrupoMat.count === 0) {
       throw new NotAcceptableException(
@@ -162,24 +152,24 @@ export class InscripcionService {
     }
 
     //SACAMOS LAS MATERIAS QUE PUDO INSCRIBIR PARA DESPUES METERLAS A LA BOLETA
-    const materiasInscritas = await prisma.detalleInsGrupoMat.findMany({
+    const materiasInscritas = await prisma.detalleInsOferta.findMany({
       where: { detalleInscripcionId: createDetalleInscripcion.id },
-      include: { grupoMateria: true },
+      include: { OfertaGrupoMateria: { include: { GrupoMateria: true } } },
     });
 
     //AHORA A LAS MATERIAS INSCRITAS HAY QUE DISMINUIRLES UN CUPO
     const materiasConCupo = materiasInscritas.filter(
-      (materia) => materia.grupoMateria.cupos > 0,
+      (materia) => materia.OfertaGrupoMateria.GrupoMateria.cupos > 0,
     );
     for (const materia of materiasConCupo) {
       await prisma.grupoMateria.update({
-        where: { id: materia.grupoMateria.id },
+        where: { id: materia.OfertaGrupoMateria.GrupoMateria.id },
         data: { cupos: { decrement: 1 } },
       });
     }
 
     await this.mapearMateriasInscritasABoletaEnTransaccion(
-      materiasInscritas.map((mi) => mi.grupoMateria),
+      materiasInscritas.map((mi) => mi.OfertaGrupoMateria.GrupoMateria),
       foundStudentId,
       prisma,
     );
@@ -221,113 +211,4 @@ export class InscripcionService {
       );
     }
   }
-
-  async crearDetallesDeInscripcion(
-    createInscripcionDto: CreateInscripcionDto,
-    fichaInscripcionId: string,
-    foundStudentId: string,
-  ) {
-    //CREAMOS UN DETALLE DE INSCRIPCION NUEVO SIN IMPORTAR SI EL ALUMNO YA INSCRIBIO ANTES
-    const createDetalleInscripcion =
-      await this.prismaService.detalleInscripcion.create({
-        data: {
-          fichaInscripcionId: fichaInscripcionId,
-          tipo: 'INSCRIPCION',
-        },
-      });
-
-    if (!createDetalleInscripcion) {
-      throw new NotAcceptableException('Error al crear la ficha');
-    }
-
-    // BUSCAMOS LAS MATERIAS QUE QUEREMOS INSCRIBIR
-    const materias = await this.prismaService.grupoMateria.findMany({
-      where: {
-        id: { in: createInscripcionDto.materiasId },
-      },
-    });
-
-    if (materias.length !== createInscripcionDto.materiasId.length) {
-      throw new NotFoundException(
-        'Alguna de las materias seleccionadas no existe',
-      );
-    }
-
-    // FILTRAMOS LAS MATERIAS CON CUPO DISPONIBLE Y DE UNA CONSTRUIMOS EL PAYLOAD
-    const payload = materias
-      .filter((materia) => materia.cupos > 0)
-      .map((materia) => ({
-        detalleInscripcionId: createDetalleInscripcion.id,
-        grupoMateriaId: materia.id,
-        estado: 'INSCRITA',
-      }));
-
-    if (payload.length === 0) {
-      throw new NotAcceptableException('Ninguna materia tiene cupo disponible');
-    }
-
-    //INSCRIBIMOS TODAS LAS MATERIAS DE UN SAQUE
-    const createDetalleInsGrupoMat =
-      await this.prismaService.detalleInsGrupoMat.createMany({
-        data: payload,
-      });
-
-    if (createDetalleInsGrupoMat.count === 0) {
-      throw new NotAcceptableException(
-        'No se pudieron inscribir las materias, error de concurrencia en los cupos o similar',
-      );
-    }
-
-    //SACAMOS LAS MATERIAS QUE PUDO INSCRIBIR PARA DESPUES METERLAS A LA BOLETA
-    const materiasInscritas =
-      await this.prismaService.detalleInsGrupoMat.findMany({
-        where: { detalleInscripcionId: createDetalleInscripcion.id },
-        include: { grupoMateria: true },
-      });
-
-    //AHORA A LAS MATERIAS INSCRITAS HAY QUE DISMINUIRLES UN CUPO
-    const materiasConCupo = materiasInscritas.filter(
-      (materia) => materia.grupoMateria.cupos > 0,
-    );
-    for (const materia of materiasConCupo) {
-      await this.prismaService.grupoMateria.update({
-        where: { id: materia.grupoMateria.id },
-        data: { cupos: { decrement: 1 } },
-      });
-    }
-
-    await this.mapearMateriasInscritasABoleta(
-      materiasInscritas.map((mi) => mi.grupoMateria),
-      foundStudentId,
-    );
-  }
-
-  async mapearMateriasInscritasABoleta(
-    materiasInscritas: GrupoMateria[],
-    foundStudentId: string,
-  ) {
-    //BUSCAMOS LA BOLETA DEL ALUMNO
-    const foundBoleta = await this.prismaService.boletaInscripcion.findFirst({
-      where: { estudianteId: foundStudentId },
-    });
-
-    if (!foundBoleta) {
-      throw new NotFoundException('Boleta no encontrada');
-    }
-
-    //MAPEAMOS LAS MATERIAS INSCRITAS A LA BOLETA
-    const createBoletaGrupoMateria =
-      await this.prismaService.boletaGrupoMateria.createMany({
-        data: materiasInscritas.map((materia) => ({
-          boletaInscripcionId: foundBoleta.id,
-          grupoMateriaId: materia.id,
-        })),
-      });
-
-    if (createBoletaGrupoMateria.count === 0) {
-      throw new NotAcceptableException(
-        'No se pudieron crear las materias en la boleta',
-      );
-    }
-  } */
 }
